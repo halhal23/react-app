5bf208157a9ba57f48e803e64c8da9f8476825aa
Hiroharu Hara <hh.25799623@gmail.com>
2020-09-13 23:38:43 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 function App() {
+  const profiles = [
+    { name: "fujia", age: 43 },
+    { name: "hiroharu", age: 23 },
+    { name: "", age: 43 }
+  ]
   return (
     <React.Fragment>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </React.Fragment>
   )
 }
 
-const Cat = () => {
-  return <div>meow meow</div>
+const User = (props) => {
+  return <div>Hello my name is { props.name }. I am { props.age } years old.</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string.isRequired
 }
 
 export default App;
