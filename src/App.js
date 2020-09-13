ad2d4a48bb078d77aeb4a06234baa922b94319bc
Hiroharu Hara <hh.25799623@gmail.com>
2020-09-13 14:16:50 +0900

    learn more jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 function App() {
-  return <div>hello world welcome</div>;
+  return <input type="text" onClick={() => {console.log('クリック')}} />;
 }
 
 export default App;
